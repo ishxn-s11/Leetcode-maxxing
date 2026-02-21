83ed5fa229e8d1dd49af6422bc6b7119901338e8
Ishan Singh <ishan29.s2908@gmail.com>
2026-02-22 00:19:04 +0530

    Add files via upload

---
+++ 53.Maximum-Subarray/max_subarray_optimal.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int maxSubArray(vector<int>&nums){
+        int maxsum=INT_MIN;
+        int sum=0,n=nums.size();
+        for(int i=0;i<n;i++){
+            sum+=nums[i];
+            if(sum>maxsum) maxsum=sum;
+            if(sum<0) sum=0;
+        }
+    return maxsum;
+    }
+
+};
