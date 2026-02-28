ed253595f312f88e403b0ae8dbff6fa126c4962c
Ishan Singh <ishan29.s2908@gmail.com>
2026-02-28 23:52:04 +0530

    Add files via upload

---
+++ 1423.-Maximum-Points-You-Can-Obtain-From-Cards/max_points_u_can_obtain.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int maxScore(vector<int>& cardPoints, int k) {
+        int lsum=0,rsum=0;
+        int maxsum=0;
+        int r_idx=cardPoints.size()-1;
+        for(int i=0;i<k;i++) lsum+=cardPoints[i];
+        maxsum=lsum;
+        for(int i=k-1;i>=0;i--){
+            lsum-=cardPoints[i];
+            rsum+=cardPoints[r_idx];
+            r_idx--;
+            maxsum=max(maxsum,lsum+rsum);
+        }
+        return maxsum;
+    }
+};
