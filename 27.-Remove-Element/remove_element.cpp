38e72935e60175d058ac9aa74ce752254768c917
Ishan Singh <ishan29.s2908@gmail.com>
2026-03-03 22:25:39 +0530

    Add files via upload

---
+++ 27.-Remove-Element/remove_element.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int removeElement(vector<int>& nums, int val) {
+        int i=0,k=0;
+        while(i<nums.size()){
+            if(nums[i]!=val){
+                nums[k]=nums[i];
+                k++;
+            }
+            i++;
+        }
+        return k;
+    }
+};
