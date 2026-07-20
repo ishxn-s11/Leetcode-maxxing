c14d9858ff81c0b6dd740dc013f5872317bd1bae
Ishan Singh <ishan29.s2908@gmail.com>
2026-07-21 00:53:05 +0530

    Create merge2lsts21.cpp

---
+++ 21. Merge Two Sorted Lists/merge2lsts21.cpp
@@ -0,0 +1,30 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* mergeTwoLists(ListNode* list1, ListNode* list2) {
+        ListNode temp(0);
+        ListNode*t=&temp;
+        while(list1!=nullptr && list2!=nullptr){
+            if(list1->val<=list2->val){
+                t->next=list1;
+                list1=list1->next;
+            }else{
+                t->next=list2;
+                list2=list2->next;
+            }
+            t=t->next;
+        }
+        if(list1!=nullptr) t->next=list1;
+        else t->next=list2;
+        return temp.next;
+    }
+};
